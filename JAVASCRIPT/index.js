1e6623df8102cda71ebf2821cec9592ef84ec50f
salma1489 <119680235+salma1489@users.noreply.github.com>
2023-02-22 08:30:21 +0200

    Add files via upload

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,27 @@
+var Applicant =
+{
+    name : "SALAAMA",
+    surname : "MOHAMED",
+    age : 21,
+    email : "SALAAMAM@GMAIL.COM",
+    address : "22 CESSNA STREET",
+    bio : "TECH JUNKY WITH AN APPETITE FOR LIFE",
+    education : "GRADE 12",
+};
+
+//======================================================================================================
+// ARRAY AND LISTS
+//let data = ["Computer skills","AWS cloud computing practioner"];
+//
+//let list = document.getElementById("myList");
+//
+//data.forEach((item) =>{
+  //  let li = document.createElement("li");
+    //li.innerText = item;
+    //list.appendChild(li);
+//});
+ let arr= ["computer skills","AWS cloud computing practitioner"];
+
+ for(let i = 0; i< arr.length; i++){
+    console.log(arr[i]);
+ }
